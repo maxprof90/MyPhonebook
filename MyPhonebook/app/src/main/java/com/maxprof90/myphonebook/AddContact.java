d37dc0ff5c1c486baf02ef0ebd4cf08ecaf65b87
Maksym <maxprof90@gmail.com>
2021-01-11 16:45:59 +0200

    *fix activity_add_contact.xml *fix entry field

--- MyPhonebook/app/src/main/java/com/maxprof90/myphonebook/AddContact.java
+++ MyPhonebook/app/src/main/java/com/maxprof90/myphonebook/AddContact.java
@@ -3,8 +3,11 @@ package com.maxprof90.myphonebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddContact extends AppCompatActivity {
+
+    EditText fname, lname, phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
