96c9e2affca851ec9487aa76fc0a8df2e1be41c3
Maksym <maxprof90@gmail.com>
2021-01-19 20:07:54 +0200

    add recycle view in contact

--- app/src/main/java/com/maxprof90/myphonebook/fragments/FragmentContact.java
+++ app/src/main/java/com/maxprof90/myphonebook/fragments/FragmentContact.java
@@ -8,12 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.maxprof90.myphonebook.R;
+import com.maxprof90.myphonebook.adapter.RecycleViewAdapter;
+import com.maxprof90.myphonebook.model.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentContact extends Fragment {
 
     View v;
+
+    RecyclerView recyclerView;
+    List<Contact> contactList;
 
     public FragmentContact() {
     }
@@ -22,6 +32,28 @@ public class FragmentContact extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.contact_fragment, container, false);
+       recyclerView = v.findViewById(R.id.contact_recycleview);
+        RecycleViewAdapter recycleViewAdapter = new RecycleViewAdapter(getContext(), contactList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(recycleViewAdapter);
         return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        contactList = new ArrayList<>();
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
+        contactList.add(new Contact("Вася", "1234567", R.drawable.avatar));
     }
 }
